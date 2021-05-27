d85378107bc217f1072f4df8c25e12f026906758
Mrr-cxh <1361091680@qq.com>
2021-05-27 08:45:44 +0800

    更新gh-pages

--- src/plugins/axios.js
+++
@@ -1,9 +0,0 @@
-// 封装 axios
-import axios from "axios";
-import Vue from "vue";
-axios.defaults.baseURL = "https://cnodejs.org/api/v1";
-axios.interceptors.response.use(res => {
-  return res.data;
-});
-Vue.prototype.$axios = axios;
-export default axios;
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-// 导出的对象就是配置对象
-module.exports = {
-  // publicPath 公共路径，主要作用是资源的路径，就是index.html使用什么样的地址去找 css js img 等
-  // 默认值是 '/' 就是服务器根地址 假如你的服务器地址是https://<USERNAME>.github.io/<REPO>/ 那么 publicPath 默认就是https://<USERNAME>.github.io/，当你的 index.html 导入资源的时候，路径就是错误的
-  // 要区分生产环境和开发环境，只有在部署的时候也就是生产环境下需要设置 publicPath
-  publicPath: process.env.NODE_ENV === "production" ? "/Vue-Community/" : "/"
-};
